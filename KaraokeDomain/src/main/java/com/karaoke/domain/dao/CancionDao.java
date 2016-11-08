be282bfb97349a9210f97e25da36b4a98fc2a657
LuisT <LuisT@DESKTOP-OJA2EMD>
2016-11-07 22:36:09 -0500

    Borrando las capas de Business, Dominio y Service Iniciales

--- KaraokeDomain/src/main/java/com/karaoke/domain/dao/CancionDao.java
+++
@@ -1,7 +0,0 @@
-package com.karaoke.domain.dao;
-
-import com.karaoke.domain.entity.Cancion;
-
-public interface CancionDao extends GenericDao<Cancion>{
-	//public Cancion retrieveByNombre(String nombre);
-}
--- KaraokeServiceClient/src/main/java/com/karaoke/service/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost:8080/")
-package com.karaoke.service.client;
